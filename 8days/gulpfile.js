d70f68058429348b1eaf26a07253bf14e30f89ff
Никита Победоносцев <kidrobot@bk.ru>
2017-12-28 15:12:41 +0300

    [8Days] Arrange\minify SVGs and add svg-sprite task for interactive icons

--- 8days/gulpfile.js
+++ 8days/gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
+
+// DEV images START
 var imagemin = require('gulp-imagemin');
 var imageminGuetzli = require('imagemin-guetzli');
 var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var rename = require("gulp-rename");
 
 gulp.task('imagemin', () =>
   gulp.src('source/img-build/**/*.{png,jpg}')
@@ -17,3 +21,20 @@ gulp.task("webp", function() {
     .pipe(webp({ quality: 60 }))
     .pipe(gulp.dest("img/webp"));
 });
+
+gulp.task("svgo", function() {
+  gulp.src("source/svg/*.svg")
+    .pipe(imagemin([
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("img/svg"));
+});
+
+gulp.task("sprite", function() {
+  gulp.src("img/svg/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("img/svg"));
+});
+
+// DEV images END
