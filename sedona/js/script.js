4b25e54ac054199d144414e784e6e8d78eadc8dd
Nikita PB <npb.nikita@gmail.com>
2021-04-04 14:32:45 +0300

    Update info and markup

--- sedona/js/script.js
+++
@@ -1,8 +0,0 @@
-var link = document.querySelector(".find-hotel-button");
-var popup = document.querySelector(".modal-search");
-
-
-link = link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.toggle("modal-search--visible");
-});
